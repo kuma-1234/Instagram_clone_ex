8f8febc567de16203d4500bd0018d4adbd118e57
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-14 14:03:45 +0900

    image_photo → icon_imageに訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  before_validation { emial.douncase! }
+  before_validation { email.downcase! }
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  mount_uploader :image_photo, ImageUploader
+  mount_uploader :icon_image, ImageUploader
 end
