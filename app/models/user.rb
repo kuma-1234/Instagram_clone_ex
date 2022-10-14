1c87afe53eadd8073b4b44f1946708e3ff842816
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-14 13:24:17 +0900

    誤字の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   before_validation { emial.douncase! }
   validates :name, presence: true, length: { maximum: 20 }
-  validates :email, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 100 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
-  validates :password, length { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  mount_uploader :image_photo, ImageUploader
 end
