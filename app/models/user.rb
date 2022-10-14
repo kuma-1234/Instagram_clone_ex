56313c9b7e304ef1e628f041b4117f62938cad5a
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-14 11:10:24 +0900

    validation設定の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  before_validation { emial.douncase! }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 100 }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
