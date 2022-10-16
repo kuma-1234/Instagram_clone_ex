da7ebfe2a74101e9379477cb832df14d27a76f57
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-16 23:06:06 +0900

    blogのconfirmルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :sessions, only:[ :new, :create, :destroy ]
   resources :users
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :collection
+    end
+  end
 end
