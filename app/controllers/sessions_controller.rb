b1f3072ce9234e981b33d5fd2e82068ad5954602
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-16 19:43:21 +0900

    blogモデルの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user.id)
+    session.delete(:user_id)
     flash[:notice] = "ログアウトしました！"
     redirect_to new_session_path
   end
