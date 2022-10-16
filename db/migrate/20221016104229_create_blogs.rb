82cf15386893433902cac06b1490c790a556828e
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-16 20:15:22 +0900

    blogテーブルの作成、マイグレーション

--- db/migrate/20221016104229_create_blogs.rb
+++ db/migrate/20221016104229_create_blogs.rb
@@ -1,7 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[6.1]
   def change
     create_table :blogs do |t|
-
+      t.text :content
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_14_020430) do
+ActiveRecord::Schema.define(version: 2022_10_16_104229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
