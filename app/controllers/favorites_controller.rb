db81ef84ddd32649d95c98b46ba39ccb162723f4
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-17 22:23:38 +0900

    favoriteコントローラーの設定

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+
+  def create
+    favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    redirect_to blogs_path
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to blogs_path
+  end
+
+  def show
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
+end
