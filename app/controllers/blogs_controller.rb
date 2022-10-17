330ba4c877d0b5109a2dca8ab8d3cb76ca61e099
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-17 11:14:48 +0900

    各アクションの設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,60 @@
 class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = current_user.blogs.build(blog_params)
+    if params[:back]
+    render :new
+    else
+      if @blog.save
+        redirect_to new_blogs_path
+      else
+        render :new
+      end
+    end
+  end
+
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+      if @blog.update(blog_params)
+        redirect_to blogs_path, notice: "投稿しました！"
+      else
+        render :edit
+      end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:content, :image, :image_cache)
+  end
 end
+
+
+
